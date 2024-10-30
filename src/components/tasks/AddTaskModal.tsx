b665e823de502faec1c0c09dd2eb782ee65bf196
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-10-30 11:33:30 -0600

    feat(components/tasks):TaskCard.tsx, TaskForm.tsx, and TaskList.tsx were added

--- src/components/tasks/AddTaskModal.tsx
+++ src/components/tasks/AddTaskModal.tsx
@@ -6,14 +6,60 @@ import {
     Transition,
     TransitionChild,
 } from "@headlessui/react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import TaskForm from "./TaskForm";
+import { TaskFormData } from "@/types/index";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createTask } from "@/api/TaskAPI";
+import { toast } from "react-toastify";
 
 export default function AddTaskModal() {
+    // NOTE: If modal exists
     const navigate = useNavigate();
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const modalTask = queryParams.get("newTask");
     const show = modalTask ? true : false;
+
+    // NOTE: Get projectId
+    const params = useParams();
+    const projectId = params.projectId!;
+
+    const initialValues: TaskFormData = { name: "", description: "" };
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({
+        defaultValues: initialValues,
+    });
+
+    const queryClient = useQueryClient();
+
+    const { mutate } = useMutation({
+        mutationFn: createTask,
+        onError: (error) => {
+            toast.error(error.message);
+        },
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({
+                queryKey: ["editProject", projectId],
+            });
+            toast.success(data);
+            reset();
+        },
+    });
+
+    const handleCreateTask = (formData: TaskFormData) => {
+        const data = {
+            formData,
+            projectId,
+        };
+        mutate(data);
+    };
 
     return (
         <>
@@ -53,15 +99,32 @@ export default function AddTaskModal() {
                                         as="h3"
                                         className="font-black text-4xl  my-5"
                                     >
-                                        Nueva Tarea
+                                        New task
                                     </DialogTitle>
 
                                     <p className="text-xl font-bold">
-                                        Llena el formulario y crea {""}
+                                        Fill out the form and {""}
                                         <span className="text-fuchsia-600">
-                                            una tarea
+                                            create a task
                                         </span>
                                     </p>
+                                    <form
+                                        className="mt-10 space-y-3"
+                                        onSubmit={handleSubmit(
+                                            handleCreateTask
+                                        )}
+                                        noValidate
+                                    >
+                                        <TaskForm
+                                            register={register}
+                                            errors={errors}
+                                        />
+                                        <input
+                                            type="submit"
+                                            value="Save task"
+                                            className=" bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                                        />
+                                    </form>
                                 </DialogPanel>
                             </TransitionChild>
                         </div>
