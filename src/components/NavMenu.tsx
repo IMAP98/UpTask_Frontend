bb70e51bbffec34e06cf1dc47257369c8cb6fd5f
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-11-11 15:49:19 -0600

    feat: logout working

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -8,12 +8,20 @@ import {
 import { Bars3Icon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 import { User } from "../types";
+import { useQueryClient } from "@tanstack/react-query";
 
 type NavMenuProps = {
     name: User["name"];
 };
 
 export default function NavMenu({ name }: NavMenuProps) {
+    const queryClient = useQueryClient();
+
+    const logout = () => {
+        localStorage.removeItem("UPTASK_AUTH_TOKEN");
+        queryClient.invalidateQueries({ queryKey: ["user"] });
+    };
+
     return (
         <Popover className="relative">
             <PopoverButton className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400">
@@ -47,7 +55,7 @@ export default function NavMenu({ name }: NavMenuProps) {
                         <button
                             className="block p-2 hover:text-purple-950"
                             type="button"
-                            onClick={() => {}}
+                            onClick={logout}
                         >
                             Logout
                         </button>
