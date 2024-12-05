1d6ce92d1a7b66e61ebfa79646074eeb8f1fbc8f
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-12-05 14:39:50 -0600

    fix vercel commit

--- src/views/404/NotFound.tsx
+++ src/views/404/NotFound.tsx
@@ -6,7 +6,7 @@ export const NotFound = () => {
             <h1 className="font-black text-center text-4xl text-white">
                 Page not found
             </h1>
-            <p className="mt-10 text-center text-white">
+            <p className="mt-10 text-center  text-white">
                 You may want to go back to{" "}
                 <Link to="/" className="text-fuchsia-500">
                     Projects
